c455ed62dc2520486653e564a5836234d6a8b36c
Michael Gaudreau <gaudreaum01@gmail.com>
2020-07-10 13:12:11 -0400

    Add a default state to the Timer component

--- src/Timer.js
+++ src/Timer.js
@@ -1,5 +1,13 @@
 import React, { Component } from 'react';
 class Timer extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      hours: 0,
+      minutes: 5,
+      seconds: 0
+    }
+  }
   render() {
     return (
       <div>
