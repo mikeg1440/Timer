33e69d8d681461c3a7648d51185e233675eba0c0
Michael Gaudreau <gaudreaum01@gmail.com>
2020-07-10 13:10:20 -0400

    Create a TimeForm componenet to set the timer settings

---
+++ src/TimeForm.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class TimeForm extends Component {
+  render() {
+    return (
+      <div className='modal' id='time-form'>
+      </div>
+    );
+  }
+
+}
+
+export default TimeForm;
