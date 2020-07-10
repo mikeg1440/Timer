d8dea554a44073a206e046edb96a3cc1779d8902
Michael Gaudreau <gaudreaum01@gmail.com>
2020-07-10 17:10:02 -0400

    Refactor the App component to remove the generated code and replace with new CSS classes

--- src/App.js
+++ src/App.js
@@ -6,21 +6,10 @@ import Timer from './Timer';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <div className="app-container">
+        <Timer />
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <Timer />
+      </div>
     </div>
   );
 }
