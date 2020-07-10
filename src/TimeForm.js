6ad678d9a365c18a6517621967277556eb8c214a
Michael Gaudreau <gaudreaum01@gmail.com>
2020-07-10 13:10:49 -0400

    Add a constructor to take derive the intitial state from the props passed in

--- src/TimeForm.js
+++ src/TimeForm.js
@@ -1,6 +1,15 @@
 import React, { Component } from 'react';
 
 class TimeForm extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      hours: props.hours,
+      minutes: props.minutes,
+      seconds: props.seconds
+    }
+  }
+
   render() {
     return (
       <div className='modal' id='time-form'>
